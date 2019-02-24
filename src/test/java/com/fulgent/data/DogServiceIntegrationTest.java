a4b6e48d3f18f58048753a0dd418c43760ca1b73
hwandong <hwandong@yahoo.com>
2019-02-24 13:50:22 -0800

    Updated tests.

---
+++ src/test/java/com/fulgent/data/DogServiceIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.fulgent.data;
+
+import com.fulgent.data.repository.DogRepository;
+import com.fulgent.data.repository.UserFavoritedRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+
+public class DogServiceIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TestEntityManager entityManager;
+
+    @MockBean
+    private DogRepository dogRepository;
+
+    @Autowired
+    private UserFavoritedRepository dogFavoriteRepository;
+
+    @Test
+    public void retrieveDogs_basic() throws Exception {
+        /**
+         * TODO:
+         */
+    }
+
+}
---
+++ src/test/java/com/fulgent/data/DogServiceTest.java
@@ -0,0 +1,104 @@
+package com.fulgent.data;
+
+import com.fulgent.data.repository.entity.DogEntity;
+import com.fulgent.data.repository.DogRepository;
+import com.fulgent.data.repository.UserFavoritedRepository;
+import com.fulgent.data.rest.model.ApplicationMessage;
+import com.fulgent.data.rest.model.VoteUpDown;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+// unit testing
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DataJpaTest
+public class DogServiceTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private DogRepository dogRepository;
+
+    @Autowired
+    private UserFavoritedRepository dogFavoriteRepository;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    //public List<DogEntity> findDogsByBreed(@Param("breed") String breed);
+    @Test
+    public void whenFindByBreed_thenReturnDogs() {
+        // given
+        DogEntity dog = new DogEntity("pug", "http://i.imgur.com/ozJD7SC.png","pug desc");
+        entityManager.persist(dog);
+        entityManager.flush();
+
+        // when
+        List<DogEntity> foundDogsList = dogRepository.findDogsByBreed(dog.getBreed());
+
+        // then
+        assertThat(foundDogsList.get(0).getBreed()).isEqualTo(dog.getBreed());
+    }
+
+    //public List<DogEntity> findDogsGroupByBreed();
+    @Test
+    public void whenFindGroupsByBreed_thenReturnDogs() {
+        // given
+        DogEntity dog1 = new DogEntity("pug", "http://i.imgur.com/ozJD7SC.png","pug desc");
+        DogEntity dog2 = new DogEntity("labrador", "http://i.imgur.com/eE29vX4.png","labrador desc");
+        entityManager.persist(dog1);
+        entityManager.persist(dog2);
+        entityManager.flush();
+
+        // when
+        List<DogEntity> foundDogsGroups = dogRepository.findDogsGroupByBreed();
+
+        // then
+        assertThat(foundDogsGroups.size()).isEqualTo(2);
+    }
+
+    //public DogEntity findDogDetail(@Param("123") long dogId);
+    @Test
+    public void whenFindDetailById_thenReturnDogs() {
+        // given
+        DogEntity dog = new DogEntity("pug", "http://i.imgur.com/ozJD7SC.png","TEST");
+        long dogId = dog.getDogId();
+        entityManager.persist(dog);
+        entityManager.flush();
+
+        // when
+        DogEntity foundDog = dogRepository.findDogDetail(dogId);
+
+        // then
+        assertThat(foundDog.getDetails()).isEqualTo(dog.getDetails());
+    }
+
+    //public ApplicationMessage voteUpDown(@RequestBody VoteUpDown voteUpDown);
+    @Test
+    public void whenVoteUpDown_thenReturnVotes() {
+        // given
+        DogEntity dog = new DogEntity("pug", "http://i.imgur.com/ozJD7SC.png","TEST");
+        long dogId = dog.getDogId();
+        entityManager.persist(dog);
+        entityManager.flush();
+
+        VoteUpDown vote = new VoteUpDown("user123", dogId, Boolean.TRUE);
+        // when
+        ApplicationMessage voteDog = dogFavoriteRepository.voteUpDown(vote);
+
+        // then
+        assertThat(voteDog.getMessage()).isEqualTo("success");
+    }
+}
+
